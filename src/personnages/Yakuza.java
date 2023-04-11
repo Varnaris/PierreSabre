719831195f90575e8d8a9ba372e13124a9e92114
Varnaris <elouan.vuichard@gmail.com>
2023-04-11 19:18:00 +0200

    Yakusa clan

--- src/personnages/Yakuza.java
+++ src/personnages/Yakuza.java
@@ -32,4 +32,9 @@ public class Yakuza extends Humain{
 			parler("Ce ronin pensait vraiment battre " + getNom() + " du clan de " + clan + " ? "
 					+ "Je l'ai dépouillé de ses " + gain + " sous.");
 		}
+	 @Override
+	 public void direBonjour() {
+		 super.direBonjour();
+		 parler("Mon clan est celui de " + clan +".");
+	 }
 }
